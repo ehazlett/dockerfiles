08b671f5f68131fd394086ca1f4ce688f1ea9c12
Evan Hazlett <ejhazlett@gmail.com>
2015-04-12 15:17:57 -0400

    added proxy
    Signed-off-by: Evan Hazlett <ejhazlett@gmail.com>

---
+++ proxy/run.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+UPSTREAMS=${1:-""}
+
+show_usage() {
+    echo "Usage: $0 [upstreams]"
+}
+
+if [ -z "$UPSTREAMS" ]; then
+    show_usage
+    exit 1
+fi
+
+echo "configuring proxy"
+
+CONF="
+user  nginx;
+worker_processes  1;
+daemon off;
+
+error_log  /var/log/nginx/error.log warn;
+pid        /var/run/nginx.pid;
+
+
+events {
+    worker_connections  1024;
+}
+
+
+http {
+    include       /etc/nginx/mime.types;
+    default_type  application/octet-stream;
+
+    upstream up {
+"
+
+for UP in $UPSTREAMS; do
+    echo "adding upstream $UP"
+    CONF="$CONF
+        server $UP;
+"
+done
+    CONF="$CONF
+    }
+
+    log_format  main  '\$remote_addr - \$remote_user [\$time_local] \"\$request\" '
+                      '\$status \$body_bytes_sent \"\$http_referer\" '
+                      '\"\$http_user_agent\" \"\$http_x_forwarded_for\"';
+
+    access_log  /var/log/nginx/access.log  main;
+
+    sendfile        on;
+    #tcp_nopush     on;
+
+    keepalive_timeout  65;
+
+"
+
+CONF="$CONF
+    server {
+        listen 80;
+        location / {
+            proxy_pass http://up;
+        }
+    }
+}
+"
+
+echo "$CONF" > /etc/nginx/nginx.conf
+
+echo "proxy running"
+exec nginx -c /etc/nginx/nginx.conf

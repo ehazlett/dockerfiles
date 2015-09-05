66bfc79c4809471c4f610edbe893e6505848e1eb
Evan Hazlett <ejhazlett@gmail.com>
2015-09-05 18:57:00 -0400

    added curl; updated docker-proxy
    Signed-off-by: Evan Hazlett <ejhazlett@gmail.com>

--- docker-proxy/run.sh
+++ docker-proxy/run.sh
@@ -3,6 +3,14 @@ SSL_CA=${SSL_CA:-}
 SSL_CERT=${SSL_CERT:-}
 SSL_KEY=${SSL_KEY:-}
 
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+    echo "Usage:"
+    echo "  By default, it will bind mount to the Docker socket"
+    echo "  To enable TLS, pass the SSL_CA, SSL_CERT and SSL_KEY environment"
+    echo "  variables to the paths of the certificates."
+    exit 1
+fi
+
 echo "Listening on $PORT"
 if [ ! -z "$SSL_CA" ] && [ ! -z "$SSL_CERT" ] && [ ! -z "$SSL_KEY" ]; then
     echo "Using TLS"

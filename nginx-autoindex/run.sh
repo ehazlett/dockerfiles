6cbfcb6d08a6a642644df76d49a447ba2a50d506
Evan Hazlett <ejhazlett@gmail.com>
2017-10-21 19:34:52 -0400

    update nginx autoindex
    Signed-off-by: Evan Hazlett <ejhazlett@gmail.com>

--- nginx-autoindex/run.sh
+++ nginx-autoindex/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ROOT_DIR=${ROOT_DIR:-/usr/share/nginx/html}
-sed -i 's@ROOT_PLACEHOLDER@'"$ROOT_DIR"'@' /etc/nginx/conf.d/default.conf
+sed -i 's@ROOT_PLACEHOLDER@'"$ROOT_DIR"'@' /etc/nginx/nginx.conf
 nginx -g "daemon off;" -c /etc/nginx/nginx.conf
